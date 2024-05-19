1bee08bc1345090bb2cefbe346c14af6e46e1646
sheep1008 <yjhyoyo52@gmail.com>
2024-05-19 13:34:23 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int order) {
+    int answer = 0;
+    while(order>0){
+        int n=order%10;
+        if(n%3==0&&n>0)
+            answer++;
+        order/=10;
+    }
+    return answer;
+}
