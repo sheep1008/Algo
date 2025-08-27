1bbc4b1534ad4c92be536e098bec52b4fb000c49
sheep1008 <yjhyoyo52@gmail.com>
2025-08-27 23:21:54 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.57 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,14 @@
+def solution(s):
+    dic = {'a':-1, 'b':-1, 'c':-1, 'd':-1, 'e':-1, 'f':-1, 'g':-1,
+          'h':-1, 'i':-1, 'j':-1, 'k':-1, 'l':-1, 'm':-1, 'n':-1,
+          'o':-1, 'p':-1, 'q':-1, 'r':-1, 's':-1, 't':-1, 'u':-1,
+          'v':-1, 'w':-1, 'x':-1, 'y':-1, 'z':-1}
+    answer = []
+    for i, c in enumerate(s):
+        if dic[c] == -1:
+            answer.append(dic[c])
+            dic[c] = i
+        else:
+            answer.append(i - dic[c])
+            dic[c] = i
+    return answer
