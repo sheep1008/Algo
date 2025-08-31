e631d45236799744bab55c43c357a93614499f6b
sheep1008 <yjhyoyo52@gmail.com>
2025-08-31 22:33:07 +0900

    [level 2] Title: 숫자의 표현, Time: 3.94 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n//2+1):
+        s = i
+        for j in range(i+1, n//2+2):
+            s += j
+            if s == n:
+                answer += 1
+                break
+            elif s > n:
+                break
+    answer += 1
+    return answer
