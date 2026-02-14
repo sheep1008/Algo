3b12b69099ab8ca3928f1b7ac2f57460dff44322
양정호 <yjhyoyo52@gmail.com>
2026-02-15 00:53:28 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,9 @@
 def solution(brown, yellow):
-    for y_height in range(1, int(yellow ** 0.5) + 1):
-        if yellow % y_height != 0:
-            continue
-        y_width = yellow // y_height
-        if y_width < y_height:
-            break
-        
-        if (y_width + 2) * (y_height + 2) == yellow + brown:
-            return [y_width + 2, y_height + 2]
+    total_area = brown + yellow
+    
+    for h in range(3, int(total_area**0.5+1)):
+        if total_area % h == 0:
+            w = total_area // h
+            
+            if (w-2) * (h-2) == yellow:
+                return [w, h]
