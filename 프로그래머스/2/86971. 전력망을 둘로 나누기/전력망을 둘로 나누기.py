4eeb140fdeb61fc28a664909a02686498be89391
양정호 <yjhyoyo52@gmail.com>
2026-02-28 23:38:59 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.91 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,16 +1,14 @@
 from collections import deque
 
-def get_node_count(start_node, n, adj, broken_edge):
-    visited = [False] * (n+1)
-    visited[start_node] = True
-    queue = deque([start_node])
+def bfs(start, n, adj):
+    visited = [False] * (n + 1)
+    queue = deque([start])
+    visited[start] = True
     count = 1
     
     while queue:
         curr = queue.popleft()
         for neighbor in adj[curr]:
-            if (curr, neighbor) == broken_edge or (neighbor, curr) == broken_edge:
-                continue
             if not visited[neighbor]:
                 visited[neighbor] = True
                 queue.append(neighbor)
@@ -20,15 +18,20 @@ def get_node_count(start_node, n, adj, broken_edge):
 def solution(n, wires):
     answer = n
     
-    adj = [[] for _ in range(n+1)]
-    for v1, v2 in wires:
-        adj[v1].append(v2)
-        adj[v2].append(v1)
+    # 1. 모든 전선을 하나씩 끊어보기
+    for i in range(len(wires)):
+        # i번째 전선을 제외한 인접 리스트 구성
+        adj = [[] for _ in range(n + 1)]
+        for j, (v1, v2) in enumerate(wires):
+            if i == j: continue # 선택된 전선은 무시 (끊기)
+            adj[v1].append(v2)
+            adj[v2].append(v1)
         
-    for v1, v2 in wires:
-        cnt = get_node_count(v1, n, adj, (v1, v2))
+        # 2. 한쪽 전력망의 송전탑 개수 세기
+        res = bfs(1, n, adj)
         
-        diff = abs(cnt - (n - cnt))
-        answer = min(answer, diff)
+        # 3. 두 전력망 개수 차이의 최솟값 갱신
+        # |한쪽 개수 - 다른쪽 개수| = |res - (n - res)|
+        answer = min(answer, abs(res - (n - res)))
         
     return answer
