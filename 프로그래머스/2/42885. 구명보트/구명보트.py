448d9a59499792fe5e7839253b708359fc666d44
양정호 <yjhyoyo52@gmail.com>
2026-03-01 17:40:39 +0900

    [level 2] Title: 구명보트, Time: 9.74 ms, Memory: 9.64 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,17 +1,17 @@
 def solution(people, limit):
-    answer = 0
-    people.sort()
-    start, end = 0, len(people)-1 
+    people.sort() # 1. 몸무게순 정렬 (필수)
     
-    while start<=end:
-        if start == end:
-            answer += 1
-            break
-        if people[start] + people[end] > limit:
-            end-=1
-            answer+=1
-        else:
-            answer += 1
-            start+=1
-            end-=1
-    return answer
+    left = 0 # 가장 가벼운 사람의 인덱스
+    right = len(people) - 1 # 가장 무거운 사람의 인덱스
+    boats = 0
+    
+    while left <= right:
+        # 2. 가장 무거운 사람 + 가장 가벼운 사람 조합 확인
+        if people[left] + people[right] <= limit:
+            left += 1 # 가벼운 사람도 태움
+        
+        # 3. 무거운 사람은 무조건 이번 보트에 탐
+        right -= 1
+        boats += 1
+        
+    return boats
