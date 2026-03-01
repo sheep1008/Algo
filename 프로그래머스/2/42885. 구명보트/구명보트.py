4effb24de08d6d9dd3abe628bbff2324362c5b8d
양정호 <yjhyoyo52@gmail.com>
2026-03-01 17:31:45 +0900

    [level 2] Title: 구명보트, Time: 10.60 ms, Memory: 9.62 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    start, end = 0, len(people)-1 
+    
+    while start<=end:
+        if start == end:
+            answer += 1
+            break
+        if people[start] + people[end] > limit:
+            end-=1
+            answer+=1
+        else:
+            answer += 1
+            start+=1
+            end-=1
+    return answer
