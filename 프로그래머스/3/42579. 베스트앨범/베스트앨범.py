4e914212b860ebfb399383efe7c7f823ddc70915
양정호 <yjhyoyo52@gmail.com>
2026-05-04 14:23:39 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,20 +1,20 @@
 def solution(genres, plays):
     genre_total = {}
-    songs_by_genre= {}
+    song_by_genre = {}
     
     for i in range(len(genres)):
         g, p = genres[i], plays[i]
         genre_total[g] = genre_total.get(g, 0) + p
-        if g not in songs_by_genre:
-            songs_by_genre[g] = []
-        songs_by_genre[g].append((p, i))
         
-    sorted_genres = sorted(genre_total.items(), key=lambda x: x[1], reverse=True)
+        if g not in song_by_genre:
+            song_by_genre[g] = []
+        song_by_genre[g].append((p, i))
+        
+    sorted_genre = sorted(genre_total.items(), key = lambda x : x[1], reverse = True)
     
     answer = []
-    for g, _ in sorted_genres:
-        songs_by_genre[g].sort(key=lambda x: (-x[0], x[1]))
+    for g, _ in sorted_genre:
+        song_by_genre[g].sort(key=lambda x:(-x[0], x[1]))
         
-        answer.extend([idx for p, idx in songs_by_genre[g][:2]])
-        
+        answer.extend([i for _, i in song_by_genre[g][:2]])
     return answer
