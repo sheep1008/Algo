a97f23362b5a9229986c38dba1cd83d0ac0fccce
양정호 <yjhyoyo52@gmail.com>
2026-05-05 14:35:29 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.83 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -6,21 +6,22 @@ def solution(jobs):
         jobs_with_idx.append([jobs[i][0], jobs[i][1], i])
     jobs_with_idx.sort()
     
-    now, i, answer = 0, 0, 0
-    count = 0
-    wait_jobs = []
+    answer, now, i = 0, 0, 0
+    wait_queue = [] # 힙(우선순위)
+    count = 0 # 마친 작업 수
     
     while count < len(jobs):
-        while i < len(jobs) and jobs_with_idx[i][0]<=now:
-            heapq.heappush(wait_jobs, [jobs_with_idx[i][1], jobs_with_idx[i][0], jobs_with_idx[i][1]])
-            i+=1
-        
-        if wait_jobs:
-            d, r, index = heapq.heappop(wait_jobs)
-            now += d
-            answer += (now - r)
+        while i < len(jobs) and jobs_with_idx[i][0] <= now:
+            heapq.heappush(wait_queue, [jobs_with_idx[i][1], jobs_with_idx[i][0], jobs_with_idx[i][2]])
+            i += 1
+            
+        if wait_queue:
+            duration, request_time, idx = heapq.heappop(wait_queue)
+            now += duration
+            answer += (now - request_time)
             count += 1
+        
         else:
             now = jobs_with_idx[i][0]
             
-    return answer//len(jobs)
+    return answer // len(jobs)
