933537eed8d933c176b72d80c97b50bf1f02792c
양정호 <yjhyoyo52@gmail.com>
2026-05-06 16:25:34 +0900

    [level 1] Title: 모의고사, Time: 5.61 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,23 @@
 def solution(answers):
-    student = [0, 0, 0]
-    a = [1, 2, 3, 4, 5]
-    b = [2, 1, 2, 3, 2, 4, 2, 5]
-    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    scores = [0, 0, 0]
     
-    r = []
-    for i, answer in enumerate(answers):
-        if answer == a[i%len(a)]:
-            student[0] += 1
-        if answer == b[i%len(b)]:
-            student[1] += 1
-        if answer == c[i%len(c)]:
-            student[2] += 1
-    max_score = max(student)
-    for i in range(len(student)):
-        if student[i] == max_score:
-            r.append(i+1)
-    return r
+    for i in range(len(answers)):
+        if answers[i] == p1[i%len(p1)]:
+            scores[0] += 1
+        if answers[i] == p2[i%len(p2)]:
+            scores[1] += 1
+        if answers[i] == p3[i%len(p3)]:
+            scores[2] += 1
+            
+    max_score = max(scores)
+    
+    result = []
+    
+    for i in range(3):
+        if scores[i] == max_score:
+            result.append(i+1)
+            
+    return result
