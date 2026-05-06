71a36031b5e893e9871259b35c6b1c7fa991c720
양정호 <yjhyoyo52@gmail.com>
2026-05-06 14:16:38 +0900

    [level 3] Title: 이중우선순위큐, Time: 99.51 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -3,29 +3,36 @@ import heapq
 def solution(operations):
     min_h, max_h = [], []
     counts = {}
-    
+
     for op in operations:
-        command, val = op.split()
-        num = int(val)
-        
+        command, num = op.split()
+        val = int(num)
+
         if command == 'I':
-            heapq.heappush(min_h, num)
-            heapq.heappush(max_h, -num)
-            counts[num] = counts.get(num, 0) + 1
-        elif num == 1:
+            heapq.heappush(min_h, val)
+            heapq.heappush(max_h, -val)
+            counts[val] = counts.get(val, 0) + 1
+
+        elif val == 1:
             while max_h and counts.get(-max_h[0], 0) == 0:
                 heapq.heappop(max_h)
             if max_h:
-                target = -heapq.heappop(max_h)
-                counts[target] -= 1
+                n = -heapq.heappop(max_h)
+                counts[n] -= 1
+
         else:
             while min_h and counts.get(min_h[0], 0) == 0:
                 heapq.heappop(min_h)
             if min_h:
-                target = heapq.heappop(min_h)
-                counts[target] -= 1
-    while max_h and counts.get(-max_h[0], 0) == 0: heapq.heappop(max_h)
-    while min_h and counts.get(min_h[0], 0) == 0: heapq.heappop(min_h)
+                n = heapq.heappop(min_h)
+                counts[n] -= 1
+
+    while max_h and counts.get(-max_h[0], 0) == 0:
+        heapq.heappop(max_h)
+    while min_h and counts.get(min_h[0], 0) == 0:
+        heapq.heappop(min_h)
+
     if not min_h or not max_h:
-        return [0,0]
+        return [0, 0]
+
     return [-max_h[0], min_h[0]]
