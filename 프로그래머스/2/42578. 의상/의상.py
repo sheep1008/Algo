7be4b3eca0fdb152b5724c69117e90c611f29a90
양정호 <yjhyoyo52@gmail.com>
2026-05-07 22:03:57 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,16 @@
 def solution(clothes):
-    data = {}
-    
-    for k, v in clothes:
-        data[v] = data.get(v, 0) + 1
-    
+    hash_map = {}
+    arr = []
     answer = 1
-    
-    for n in data.values():
-        answer *= (n+1)
+    for c in clothes:
+        a, b = c
+        
+        hash_map[b] = hash_map.get(b, 0) + 1
+        
+    for n in hash_map.values():
+        arr.append(n)
+        
+    for a in arr:
+        answer *= (a+1)
     
     return answer - 1
