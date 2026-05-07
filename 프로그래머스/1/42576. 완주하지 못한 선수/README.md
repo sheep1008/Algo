7372e1d710e86daeb1b555271346de451fe3cf26
양정호 <yjhyoyo52@gmail.com>
2026-05-07 21:46:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.56 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 33.1 MB, 시간: 40.50 ms
+메모리: 32.8 MB, 시간: 40.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 27일 21:35:40
+2026년 05월 07일 21:46:00
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,13 @@
 def solution(participant, completion):
     data = dict()
+
     for p in participant:
-        if p not in data:
-            data[p] = 1
-        else:
-            data[p] += 1
-    
+        data[p] = data.get(p, 0) + 1
+
     for c in completion:
-        data[c] -= 1
-        
-    for key, val in data.items():
-        if val == 1:
-            return key
+        if c in data.keys():
+            data[c] -= 1
+
+    for p in participant:
+        if data[p] != 0:
+            return p
